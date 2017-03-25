634d6928a4aac18bbd5f4d4f0adcf9c5aa668807
Graeme Blair <graemeblair@users.noreply.github.com>
2017-03-25 02:12:32 -0700

    declare_population working except with levels() in functions

--- R/declare_population.R
+++ R/declare_population.R
@@ -1,52 +1,14 @@
 
+#' @importFrom lazyeval lazy_dots make_call lazy_eval
 #' @export
-declare_population <- function(..., N = NULL, population_function = fabricate_data){
-
-  options <- eval(substitute(alist(...)))
-
-  # if it's levels, do one thing:
-  options_text <- paste(substitute(options))
-
-  # revenge of the JANK TOWN, i.e., check if all the options are level calls.
-  if (all(sapply(options_text, function(x)
-    startsWith(x, "level(")))) {
-    # do levels stuff, not sure what
-
-    return(do.call(declare_population_, args = c(list(N = N), as.list(options_text))))
-
-  } else {
-    # change the ones that are calls to character strings for fabricate_data_
-    is_call <- sapply(options, class) == "call"
-
-    options[is_call] <-
-      as.list(paste0(names(options[is_call]), " = ", paste(options[is_call])))
-    names(options)[is_call] <- ""
-
-    do.call(declare_population_, args = c(list(N = N), as.list(options)))
-
+declare_population <- function(population_function = fabricate_data, ...) {
+  dots <- lazyeval::lazy_dots(...)
+  mcall <- lazyeval::make_call(substitute(population_function), dots)
+  population_function_internal <- function() {
+    lazyeval::lazy_eval(mcall)
   }
+  attributes(population_function_internal) <-
+         list(call = match.call(), type = "population")
+
+  return(population_function_internal)
 }
-
-
-#' @importFrom DDfabricate fabricate_data_ level
-#' @export
-declare_population_ <-
-  function(..., N = NULL, population_function = fabricate_data_) {
-    ## pull in the options for the population_function
-    options <- list(...)
-
-    ## create a function to return a population
-    population_function_internal <- function() {
-      ## the function runs the population_function using the options sent to ...
-      do.call(population_function, args = c(list(N = N), options))
-    }
-
-    attributes(population_function_internal) <-
-      list(call = match.call(), type = "population")
-
-    return(population_function_internal)
-  }
-
-
-
-
--- tests/testthat/test-population.R
+++ tests/testthat/test-population.R
@@ -4,19 +4,10 @@ library(DeclareDesign)
 library(magrittr)
 
 
-declare_population(level(N = 5, gdp = rnorm(N), level_name = "regions"),
-                   level(N = 12, subways = rnorm(N, mean = gdp), level_name = "cities"))()
 
-
+# works
 declare_population(N = 10, Y1 = rnorm(N), Y2 = rnorm(N))()
 
-
-declare_population(level(N = 5, gdp = rnorm(N), level_name = "regions"),
-               level(N = 12, subways = rnorm(N, mean = gdp), level_name = "cities"))()
-
-declare_population(level(N = 5, gdp = rnorm(N), level_name = "regions"),
-               level(N = 1:5, subways = rnorm(N, mean = gdp), level_name = "cities"))()
-
-declare_population(level(N = 5, gdp = rnorm(N), level_name = "regions"),
-               level(N = sample(1:5), subways = rnorm(N, mean = gdp), level_name = "cities"))()
+declare_population(districts = level(N = 5, gdp = rnorm(N)),
+                   villages = level(N = 12, subways = rnorm(N, mean = gdp)))()
 
