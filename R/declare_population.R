67df279688b4ddbcb38a61eaf8617ca3293f5d1d
Graeme Blair <graemeblair@users.noreply.github.com>
2017-03-28 21:01:43 -0700

    cleanup

--- R/declare_population.R
+++ R/declare_population.R
@@ -3,10 +3,10 @@
 #' @importFrom lazyeval lazy_dots make_call lazy_eval
 #' @export
 declare_population <- function(population_function = DDfabricate::fabricate_data, ...) {
-  dots <- lazyeval::lazy_dots(...)
-  mcall <- lazyeval::make_call(substitute(population_function), dots)
+  dots <- lazy_dots(...)
+  mcall <- make_call(substitute(population_function), dots)
   population_function_internal <- function() {
-    lazyeval::lazy_eval(mcall)
+    lazy_eval(mcall)
   }
   attributes(population_function_internal) <-
          list(call = match.call(), type = "population")
