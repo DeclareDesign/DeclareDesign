d58aed936a8f93fb9def046511d22ede03a39f5e
Graeme Blair <graemeblair@users.noreply.github.com>
2017-03-31 08:26:28 -0700

    fix labeling

--- R/declare_estimand.R
+++ R/declare_estimand.R
@@ -2,13 +2,14 @@
 
 #' @importFrom lazyeval lazy_dots make_call lazy_eval call_modify
 #' @export
-declare_estimand <- function(..., estimand_function = default_estimand_function, label = my_estimand) {
+declare_estimand <- function(..., label = my_estimand, estimand_function = default_estimand_function) {
   dots <- lazy_dots(...)
   mcall <- make_call(substitute(estimand_function), dots)
+  label <- as.character(substitute(label))
   estimand_function_internal <- function(data) {
     mcall$expr$data <- data
     value <- lazy_eval(mcall)
-    data.frame(estimand_label = deparse(substitute(label)), estimand = value, stringsAsFactors = FALSE)
+    data.frame(estimand_label = label, estimand = value, stringsAsFactors = FALSE)
   }
   attributes(estimand_function_internal) <-
     list(call = match.call(), type = "estimand")
--- R/declare_estimator.R
+++ R/declare_estimator.R
@@ -3,13 +3,14 @@
 #' @importFrom DDestimate difference_in_means
 #' @importFrom lazyeval lazy_dots make_call lazy_eval call_modify
 #' @export
-declare_estimator <- function(..., estimator_function = DDestimate::difference_in_means, label = my_estimator) {
+declare_estimator <- function(..., label = my_estimator, estimator_function = DDestimate::difference_in_means) {
   dots <- lazy_dots(...)
   mcall <- make_call(substitute(estimator_function), dots)
+  label <- as.character(substitute(label))
   estimator_function_internal <- function(data) {
     mcall$expr$data <- data
     results <- lazy_eval(mcall)
-    data.frame(estimator_label = deparse(substitute(label)), results, stringsAsFactors = FALSE)
+    data.frame(estimator_label = label, results, stringsAsFactors = FALSE)
   }
   attributes(estimator_function_internal) <-
     list(call = match.call(), type = "estimator")
