2c558f811a4f4d4345645361e5f7b78a8c53164e
Graeme Blair <graemeblair@users.noreply.github.com>
2017-04-01 09:34:44 -0700

    rollback declare pop to make a new branch

--- R/declare_population.R
+++ R/declare_population.R
@@ -1,30 +1,14 @@
-
 #' @importFrom DDfabricate fabricate_data
-#' @importFrom rlang eval_tidy dots_quos quos UQ
+#' @importFrom lazyeval lazy_dots make_call lazy_eval
 #' @export
-declare_population <- function(##population_function = DDfabricate::fabricate_data,
-  population_function = default_population_function,
-  ...) {
-
-  args <- dots_quos(...)
-  if ("data" %in% names(formals(population_function))) {
-    ## if data is an argument of population_function
-    args <- c(args, quos(data = data))
+declare_population <- function(population_function = DDfabricate::fabricate_data, ...) {
+  dots <- lazy_dots(...)
+  mcall <- make_call(substitute(population_function), dots)
+  population_function_internal <- function() {
+    lazy_eval(mcall)
   }
-
-  population_call_internal <- dots_quos(
-    UQ(population_function)(!!!args))
-
-  population_function_internal <- function(data = NULL) {
-    eval_tidy(population_call_internal, data = list(data = data))
-  }
-
   attributes(population_function_internal) <-
-         list(call = match.call(), type = "population")
+    list(call = match.call(), type = "population")
 
   return(population_function_internal)
-}
-
-default_population_function <- function(N) {
-  data.frame(u = rnorm(N))
 }
