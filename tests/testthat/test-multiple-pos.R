5b3190ad771e4574acde8781d6d0babc0caccb74
Graeme Blair <graemeblair@users.noreply.github.com>
2017-04-03 19:07:30 -0700

    multiple POS test

--- tests/testthat/test-multiple-pos.R
+++ tests/testthat/test-multiple-pos.R
@@ -1,6 +1,6 @@
 library(DeclareDesign)
 
-## would be nice to do this for formula, fixed POs, and all combinations
+## would be nice to do with fixed POs
 
 my_population <- declare_population(
   N = 100, income = rnorm(N), age = sample(18:95, N, replace = T))
@@ -10,9 +10,6 @@ my_potential_outcomes_Y <- declare_potential_outcomes(
 
 my_potential_outcomes_attrition <- declare_potential_outcomes(
   formula = R ~ rbinom(n = N, size = 1, prob = pnorm(Y_Z_0)))
-
-my_potential_outcomes_attrition <- declare_potential_outcomes(
-  formula = Y_obs ~ rbinom(n = N, size = 1, prob = pnorm(Y_Z_0)))
 
 my_assignment <- declare_assignment(m = 25)
 
