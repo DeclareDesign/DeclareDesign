ed946679c1a5185315443b66ea2482fb0925f07d
Graeme Blair <graemeblair@users.noreply.github.com>
2017-04-03 19:02:28 -0700

    multiple POs test with formulae

--- tests/testthat/test-multiple-pos.R
+++ tests/testthat/test-multiple-pos.R
@@ -1,29 +1,26 @@
 library(DeclareDesign)
 
+## would be nice to do this for formula, fixed POs, and all combinations
+
 my_population <- declare_population(
   N = 100, income = rnorm(N), age = sample(18:95, N, replace = T))
 
-my_potential_outcomes1 <- declare_potential_outcomes(
-  formula = R ~ rbinom(n = N, size = 1, prob = .1))
-my_potential_outcomes2 <- declare_potential_outcomes(
+my_potential_outcomes_Y <- declare_potential_outcomes(
   formula = Y ~ .25 * Z + .01 * age * Z)
 
-my_potential_outcomes_fixed_R <- declare_potential_outcomes(
-  R_Z_0 = rbinom(n = N, size = 1, prob = .05),
-  R_Z_1 = rbinom(n = N, size = 1, prob = .05))
+my_potential_outcomes_attrition <- declare_potential_outcomes(
+  formula = R ~ rbinom(n = N, size = 1, prob = pnorm(Y_Z_0)))
 
+my_potential_outcomes_attrition <- declare_potential_outcomes(
+  formula = Y_obs ~ rbinom(n = N, size = 1, prob = pnorm(Y_Z_0)))
 
 my_assignment <- declare_assignment(m = 25)
-my_estimand <- declare_estimand(mean(Y_Z_1 - Y_Z_0), label = ATE)
-my_estimator_dim <- declare_estimator(Y ~ Z, estimand = my_estimand)
-my_design <- declare_design(my_population(),
-                            my_potential_outcomes1,
-                            my_potential_outcomes2,
-                            my_estimand,
-                            my_assignment,
-                            reveal_outcomes,
-                            my_estimator_dim)
 
-my_population() %>% my_potential_outcomes_fixed_R %>% head
+my_design <- declare_design(my_population(),
+                            my_potential_outcomes_Y,
+                            my_potential_outcomes_attrition,
+                            my_assignment,
+                            step(reveal_outcomes(outcome_variable_name = "Y", assignment_variable_name = "Z")),
+                            step(reveal_outcomes(outcome_variable_name = "R", assignment_variable_name = "Z")))
 
 head(draw_data(my_design))
