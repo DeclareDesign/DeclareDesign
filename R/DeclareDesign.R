fd11bc838fa187cc2cf88835354ba15d01a93a7b
Graeme Blair <graemeblair@users.noreply.github.com>
2017-04-07 11:37:57 -0400

    load all DD packages; code of conduct

--- R/DeclareDesign.R
+++ R/DeclareDesign.R
@@ -7,3 +7,23 @@
 #' @importFrom utils data
 #' @name DeclareDesign
 NULL
+
+load <- c("fabricatr", "DDestimate", "randomizr")
+
+.onAttach <- function(...) {
+  needed <- load[!is_attached(load)]
+
+  if (length(needed) == 0)
+    return()
+
+  packageStartupMessage(paste0("Loading DeclareDesign: ", needed, collapse = "\n"))
+  suppressPackageStartupMessages(
+    lapply(needed, library, character.only = TRUE, warn.conflicts = FALSE)
+  )
+
+  ##declaredesign_conflicts()
+}
+
+is_attached <- function(x) {
+  paste0("package:", x) %in% search()
+}
