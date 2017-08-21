76bec2f3080147914b0ada37b493780c152ee5e8
Graeme Blair <graemeblair@users.noreply.github.com>
2017-08-21 15:51:19 -0400

    cleanup

--- R/download_design.R
+++ R/download_design.R
@@ -8,7 +8,7 @@
 #'
 #' @export
 download_design <- function(name) {
-  design_library_url_R <- "https://declaredesign.org/library/"
+  design_library_url_R <- "http://declaredesign.org/library/"
   design_URL <- paste0(design_library_url_R, name, ".rds")
   data_URL <- paste0(design_library_url_R, name, ".rdata")
   suppressWarnings(try(load(url(data_URL)), silent = TRUE)
@@ -24,7 +24,7 @@ download_design <- function(name) {
 #'
 #' @export
 download_template <- function(name) {
-  design_library_url_R <- "https://declaredesign.org/templates/"
+  design_library_url_R <- "http://declaredesign.org/templates/"
   design_URL <- paste0(design_library_url_R, name, "_template.rds")
   readRDS(gzcon(url(design_URL)))
 }
