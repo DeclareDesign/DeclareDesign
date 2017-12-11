c4776de653b838056cf53430c0a463afbe3f209d
Neal Fultz <nfultz@gmail.com>
2017-12-11 20:51:37 +0000

    Update deploy.sh
    Instead of $PKG_TARBALL, which is the output of the internal R CMD build on travis, use the packages that are explicitly built by the after_success step.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 set -o errexit -o nounset
 PKG_REPO=$PWD
 COMMIT="${TRAVIS_COMMIT:-$APPVEYOR_REPO_COMMIT}"
+
+# devtools builds are placed in parent directory
 cd ..
 
 mkdir drat
@@ -18,7 +20,8 @@ git remote add upstream "https://$GH_TOKEN@github.com/DeclareDesign/declaredesig
 git fetch upstream
 git checkout master
 
-Rscript -e "drat::insertPackage('../$PKG_TARBALL', repodir = '.')"
+## 
+Rscript -e 'lapply(dir(path="..", pattern="[.]zip$|[.]t.*z$", full.names=TRUE), drat:::insert, repodir=".")'
 
 git add *
 
