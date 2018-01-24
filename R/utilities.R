1e8e1b73b2378f39d0b1a6f53417650bc949f206
Neal Fultz <nfultz@gmail.com>
2018-01-24 19:03:04 +0000

    update to new version of helper function from fabricatr

--- R/utilities.R
+++ R/utilities.R
@@ -96,24 +96,45 @@ describe_variable <- function(x) {
   return(obj)
 }
 
-get_unique_variables_by_level <- function (data, ID_label, superset = NULL)
-{
+get_unique_variables_by_level <- function(data, ID_label, superset=NULL) {
+  # Superset contains a vector of character strings that contain variables
+  # the modify level call is going to write. Some of these may be columns
+  # in the data frame, others might not be. If superset is specified,
+  # then we definitely only want to check those variables
   if (!is.null(superset)) {
-    names_to_check = intersect(colnames(data), superset)
+    names_to_check <- intersect(colnames(data), superset)
+  } else {
+    names_to_check <- colnames(data)[-which(colnames(data) == ID_label)]
   }
-  else {
-    names_to_check = colnames(data)[-which(colnames(data) ==
-                                             ID_label)]
-  }
+
+  # It turns out the call isn't going to use any variables at all!
   if (!length(names_to_check)) {
     return("")
   }
-  level_variables = names_to_check[unname(unlist(lapply(names_to_check,
-                                                        function(i) {
-                                                          all(unlist(lapply(split(data[, i], data[, ID_label]),
-                                                                            function(x) {
-                                                                              length(unique(x)) == 1
-                                                                            })))
-                                                        })))]
+
+  # Iterate through each column of interest
+  # Per column, split that column's data into a list. The split indices come from the level indicator.
+  # Now, run a function which checks the unique length of each tranch
+  # Unlist the result to get a vector of TRUE or FALSE for each tranch of the list.
+  # If all tranches are TRUE, then the column has unique values based on the level's level.
+  # Take the results per column, unlist those, strip the names (if any) from the variables.
+  # Now extract the column names for the columns for which this was true. Return as a vector.
+
+  # Performance is around 22% faster than existing code for small dataset
+  level_variables <- names_to_check[
+    unname(unlist(lapply(
+      names_to_check,
+      function(i) {
+        all(unlist(
+          lapply(
+            split(data[, i], data[, ID_label]),
+            function(x) {
+              length(unique(x)) == 1
+            }
+          )
+        ))
+      }
+    )))
+    ]
   return(level_variables)
 }
