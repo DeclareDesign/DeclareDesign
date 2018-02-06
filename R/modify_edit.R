7dc9abe8744514d01c84fad03833bd4617bb2e30
Neal Fultz <nfultz@gmail.com>
2018-02-06 18:21:38 +0000

    Fix internal docs / dontrun

--- R/modify_edit.R
+++ R/modify_edit.R
@@ -3,11 +3,13 @@
 #' @rdname edit
 #' @keywords internal
 #' @examples
+#' \dontrun{
 #' here_i_am <- "foo"
 #' dot <- quo(here_i_am)
 #' dot2 <- clone_dot_edit_env(dot, here_i_am="Rock you like a hurricane", xyxyx="bar")
 #' eval_tidy(dot)
 #' eval_tidy(dot2)
+#' }
 clone_dot_edit_env <- function(dot, ..., to_replace=list(...)){
 
   environment(dot) <- list2env(to_replace, parent = environment(dot))
@@ -19,21 +21,23 @@ clone_dot_edit_env <- function(dot, ..., to_replace=list(...)){
 #' @rdname edit
 #' @keywords internal
 #' @examples
+#' \dontrun{
 #' N <- 50
 #'
 #' pop50 <- declare_population(N=N, noise=rnorm(N))
 #' nrow(pop50())
 #'
-#' pop100 <- clone_step_edit(pop50, N=100)
+#' pop100 <- DeclareDesign:::clone_step_edit(pop50, N=100)
 #' nrow(pop100())
 #' nrow(pop50())
 #'
+#' }
 clone_step_edit <- function(step, ..., to_replace=list(...)) {
   step_attributes <- attributes(step)
 
   step_attributes$dots[] <- lapply(step_attributes$dots, clone_dot_edit_env, to_replace=to_replace)
 
-  f <- with(step_attributes, DeclareDesign:::currydata(handler, dots, strictDataParam=!is.null(formals(step)$data)))
+  f <- with(step_attributes, currydata(handler, dots, strictDataParam=!is.null(formals(step)$data)))
   attributes(f) <- step_attributes
   f
 }
@@ -46,6 +50,8 @@ clone_step_edit <- function(step, ..., to_replace=list(...)) {
 #' @examples
 #' N <- 50
 #'
+#' \dontrun{
+#'
 #' my_design <- declare_design(
 #'   declare_population(N=N, noise=rnorm(N))
 #' )
@@ -54,6 +60,7 @@ clone_step_edit <- function(step, ..., to_replace=list(...)) {
 #' nrow(draw_data(my_design))
 #' nrow(draw_data(my_design2))
 #'
+#' }
 clone_design_edit <- function(design, ..., to_replace=list(...)){
   design[] <- lapply(design, clone_step_edit, to_replace=to_replace)
 
