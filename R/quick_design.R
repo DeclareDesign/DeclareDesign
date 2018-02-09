3a8851371e41f40495d9f57168be2e6b508cfa73
Neal Fultz <nfultz@gmail.com>
2018-02-09 18:48:36 +0000

    Redesign functionality

--- R/quick_design.R
+++ R/quick_design.R
@@ -1,19 +1,47 @@
 #' Declare a Design via a Template Function
 #'
-#' @param template An R function that takes a set of options and returns a design created by declare_design().
+#' \code{quick_design} easily generates a set of design from a template function.
+#'
+#' @param template a function which yields a design.
 #' @param expand boolean - if true, form the crossproduct of the ..., otherwise recycle them
 #' @param ... Options sent to the template.
+#'
+#' @return if set of designs is size one, the design, otherwise a `by`-list of designs.
+#'
+#' @examples
+#'
+#' d_template <- function(N=100) declare_design(declare_population(N=N))
+#'
+#' vary_n <- quick_design(d_template, N=seq(30, 100, 10))
+#'
+#' big_d <- quick_design(d_template, N=200)
+#'
+#' bigger_d <- redesign(big_d, N=1000)
 #'
 #' @importFrom rlang quo_expr quos
 #' @export
 quick_design <- function(template = NULL, expand = TRUE, ...) {
 
   template_args_matrix <- if (expand)
-      expand.grid(..., stringsAsFactors = FALSE)
-    else
-      cbind.data.frame(..., stringsAsFactors=FALSE)
+    expand.grid(..., stringsAsFactors = FALSE)
+  else
+    cbind.data.frame(..., stringsAsFactors=FALSE)
 
+  k <- nrow(template_args_matrix)
+  if(k == 1) return( template(...) )
 
-  by(template_args_matrix, 1:nrow(template_args_matrix), do.call, what=template)
+  by(template_args_matrix, 1:k, do.call, what=template, simplify = FALSE)
 
 }
+
+#'
+#' \code{redesign} quickly generates a design from an existing one by resetting design variables internally.
+#'
+#' @param a design
+#' @export
+#' @rdname quick_design
+redesign <- function(design, expand=TRUE, ...) {
+  f <- function(...) clone_design_edit(design, ...)
+  quick_design(f, expand, ...)
+}
+
---
+++ tests/testthat/test-redesign.R
@@ -0,0 +1,24 @@
+context("redesign")
+
+test_that("N not changed",{
+
+  N <- 100
+
+  d <- declare_design(declare_population(N=N))
+
+  expect_equal(N, 100)
+
+  expect_length(draw_data(d)$ID, 100)
+
+  others <- c(50, 100, 200, 100)
+  d_alt <- redesign(d, N=others)
+
+  for(i in seq_along(others)) {
+    expect_length(draw_data(d_alt[[i]])$ID, others[i])
+  }
+
+  # N itself should not be changed
+  expect_equal(N, 100)
+
+
+})
