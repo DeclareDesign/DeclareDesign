8f4d6f196475c926bedda69d76af324726d5b48c
Neal Fultz <nfultz@gmail.com>
2018-03-15 23:09:20 +0000

    fix calls on slash

--- tests/testthat/test-sugar.R
+++ tests/testthat/test-sugar.R
@@ -36,4 +36,5 @@ test_that("multiple PO / reveal", {
 test_that("slash constructors", {
   d <- declare_population(sleep) / declare_sampling() / declare_assignment()
   expect_equal(dim(draw_data(d)), c(10,6))
+  expect_equal(deparse(attr(d, "call")), "declare_population(sleep)/declare_sampling()/declare_assignment()")
 })
