5b27d17ebcc96efb43144025ffa5a8e5edbc3117
Luke Sonnet <luke.sonnet@gmail.com>
2018-04-04 09:30:16 -0700

    Update examples in docs

--- R/declare_estimand.R
+++ R/declare_estimand.R
@@ -1,7 +1,6 @@
-
-
-
 #' Declare Estimand
+#'
+#' @description Declares estimands which are the subjects of inquiry and can be estimated by an estimator
 #'
 #' @inheritParams declare_internal_inherit_params
 #'
@@ -11,33 +10,128 @@
 #'
 #' @examples
 #'
-#' my_population <- declare_population(N = 100)
+#' # First, set up the rest of a design for use below
+#' set.seed(42)
+#' my_population <- declare_population(N = 100, X = rnorm(N))
 #'
 #' my_potential_outcomes <- declare_potential_outcomes(
-#'   formula = Y ~ .25 * Z,
-#'   conditions = c(0, 1))
+#'   formula = Y ~ (.25 + X) * Z + rnorm(N),
+#'   conditions = c(0, 1)
+#' )
 #'
-#' df <- my_potential_outcomes(my_population())
+#' my_assignment <- declare_assignment(m = 50)
+#'
+#' my_reveal <- declare_reveal()
+#'
+#' design_stub <- declare_design(
+#'   my_population, my_potential_outcomes,
+#'   my_assignment, my_reveal
+#' )
+#'
+#' # Get example data to compute estimands on
+#' dat <- draw_data(design_stub)
+#'
+#' # ----------
+#' # 1. Single estimand
+#' # ----------
 #'
 #' # Use the default estimand setup to
 #' # declare an average treatment effect estimand
-#'
 #' my_estimand <- declare_estimand(ATE = mean(Y_Z_1 - Y_Z_0))
 #'
-#' my_estimand(df)
+#' my_estimand(dat)
 #'
-#' # Custom random assignment functions
+#' my_estimator <- declare_estimator(Y ~ Z, estimand = my_estimand)
+#'
+#' design_def <- declare_design(
+#'   my_population, my_potential_outcomes,
+#'   my_estimand, my_assignment, my_reveal,
+#'   my_estimator
+#' )
+#'
+#' conduct_design(design_def)
+#'
+#' # ----------
+#' # 2. Multiple estimands
+#' # ----------
+#'
+#' # You can also specify multiple estimands at a time
+#' ate <- declare_estimand(ATE = mean(Y_Z_1 - Y_Z_0))
+#' att <- declare_estimand(`E[ATT]` = mean(c(Y_Z_1 - Y_Z_0)[Z == 1]))
+#'
+#' # With multiple estimands, you can use one estimator for both...
+#' my_estimator_two <- declare_estimator(
+#'   Y ~ Z,
+#'   estimand = c(ate, att)
+#' )
+#'
+#' design_two <- declare_design(
+#'   my_population, my_potential_outcomes,
+#'   my_assignment, my_reveal, ate, att,
+#'   my_estimator_two
+#' )
+#'
+#' conduct_design(design_two)
+#'
+#' # You can also use different coefficients from an estimator for
+#' # two estimands, with a slightly different syntax
+#'
+#' # Name your estimands the coefficient name they get in your
+#' # estimator, and set `coefficients = TRUE`
+#' two_mands <- declare_estimand(
+#'   `(Intercept)` = mean(Y_Z_0),
+#'   `Z` = mean(Y_Z_1 - Y_Z_0),
+#'   coefficients = TRUE
+#' )
+#'
+#' # Specify your estimand as usual, but now set
+#' # `coefficients = TRUE`
+#' my_estimator_double <- declare_estimator(
+#'   Y ~ Z,
+#'   estimand = two_mands,
+#'   model = lm,
+#'   coefficients = TRUE
+#' )
+#'
+#' design_double <- declare_design(
+#'   my_population, my_potential_outcomes,
+#'   my_assignment, my_reveal, two_mands,
+#'   my_estimator_double
+#' )
+#'
+#' conduct_design(design_double)
+#'
+#' # ----------
+#' # 3. Custom estimands
+#' # ----------
 #'
 #' my_estimand_function <- function(data, label) {
 #'   ret <- with(data, median(Y_Z_1 - Y_Z_0))
-#'   data.frame(setNames(ret, label))
+#'   data.frame(estimand = setNames(ret, label))
 #' }
 #' my_estimand_custom <- declare_estimand(
-#'   handler = my_estimand_function, label = "medianTE")
+#'   handler = my_estimand_function,
+#'   label = "medianTE"
+#' )
 #'
-#' my_estimand_custom(df)
+#' my_estimand_custom(dat)
 #'
-
+#' # Can also use custom estimator
+#' my_estimator_function <- function(data){
+#'   data.frame(est = with(data, median(Y)))
+#' }
+#' my_estimator_custom <-
+#'   declare_estimator(handler = tidy_estimator(my_estimator_function),
+#'                     estimand = my_estimand_custom)
+#'
+#' design_cust <- declare_design(
+#'   my_population, my_potential_outcomes,
+#'   my_assignment, my_reveal, my_estimand_custom,
+#'   my_estimator_custom
+#' )
+#'
+#' conduct_design(design_cust)
+#'
 declare_estimand <- make_declarations(estimand_handler, "estimand", causal_type="estimand", default_label="my_estimand")
 
 
