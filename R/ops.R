d85cfe5243d25fdb43cb9c5e9d2042a1363df23e
Graeme Blair <graemeblair@users.noreply.github.com>
2018-06-04 12:01:12 -0400

    just delete slash; whitespace

--- R/ops.R
+++ R/ops.R
@@ -1,14 +1,10 @@
 
-
-# Slash utility is unreliable
-# #' @export
-# `/.d_par` <- function(e1,e2) {
-#   structure(
-#     append(e1, e2),
-#     class="design",
-#     call=call("/", attr(e1, "call"), attr(e2, "call")))
-# }
-
 `%i%` <- intersect
 
-`%||%` <- function(e1, e2) if(is.null(e1)) e2 else e1
+`%||%` <- function(e1, e2) {
+  if (is.null(e1)) {
+    e2 
+  } else { 
+    e1
+  }
+}
