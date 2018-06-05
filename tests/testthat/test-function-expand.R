63138d83b0f7bf3bca719a18d0354c2b36f45f2a
Graeme Blair <graemeblair@users.noreply.github.com>
2018-06-04 22:31:00 -0400

    expand_design re do

--- tests/testthat/test-function-expand.R
+++ tests/testthat/test-function-expand.R
@@ -1,21 +1,44 @@
 
+
 context("functions in templates")
 
-my_template <- function(N = 100, my_estimand_func = mean){ 
+my_template <- function(N = 100,
+                        my_estimand_func = mean) {
   my_pop <- declare_population(N = N, Y = rnorm(N))
   my_estimand <- declare_estimand(mand = my_estimand_func(Y))
   my_design <- declare_design(my_pop, my_estimand)
   my_design
 }
 
-test_that("raw templates can have functions as arguments", {
-  
-  expect_s3_class(my_template(my_estimand_func = median), "design")
-  
-})
+# debugonce(DeclareDesign:::expand_design)
+design_list <-
+  expand_design(
+    template = my_template,
+    N = c(c(20, 20, 20), c(20, 20, 20), c(20, 20, 20)),
+    my_estimand_func = c(mean, median)
+  )
 
-test_that("sending function to expand_design", {
-  prob <- expand_design(my_template, my_estimand_func = median)
-  
-  
-})
+design_list <-
+  expand_design(
+    template = my_template,
+    N = c(10, 50, 100),
+    my_estimand_func = c(mean, median)
+  )
+
+design_list <-
+  expand_design(
+    template = my_template,
+    N = c(10, 50, 100),
+    my_estimand_func = list(mean, median)
+  )
+
+
+design_list <-
+  expand_design(
+    template = my_template,
+    N = c(10, 50, 100),
+    my_estimand_func = mean
+  )
+
+
+
