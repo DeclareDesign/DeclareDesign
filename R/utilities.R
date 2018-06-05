0bb8b2981bb98beec662b849271512c3c5cb2242
Graeme Blair <graemeblair@users.noreply.github.com>
2018-06-04 22:50:10 -0400

    more whitespace

--- R/utilities.R
+++ R/utilities.R
@@ -5,32 +5,31 @@
 # step 1 will have label set to pop
 #
 #' @importFrom rlang f_text f_env
-
 maybe_add_labels <- function(quotations) {
-
+  
   labeller <- function(quotation, lbl) {
     cx <- quotation[[2]]
-
+    
     if (lbl == "") {
       lbl <- f_text(quotation)
     }
-
-    if(is.call(cx) && is.symbol(cx[[1]]) && ! "label" %in% names(cx)){
-
+    
+    if (is.call(cx) && is.symbol(cx[[1]]) && !"label" %in% names(cx)) {
+      
       f <- get0(as.character(cx[[1]]), f_env(quotation), "function") #match.fun does not repect quosures environment, doing get manually
-      if("declaration" %in% class(f) && "label" %in% names(formals(f))){
+      if ("declaration" %in% class(f) && "label" %in% names(formals(f))) {
         quotation[[2]][["label"]] <- lbl
       }
     }
     quotation
   }
-
-  for(i in seq_along(quotations)){
-    if(names(quotations)[i] == "")
+  
+  for (i in seq_along(quotations)) {
+    if (names(quotations)[i] == "")
       names(quotations)[i] <- f_text(quotations[[i]])
     quotations[[i]] <- labeller(quotations[[i]], names(quotations)[i])
   }
-
+  
   if (any(duplicated(names(quotations)))) {
     stop(paste0("Please provide unique names for each design step. Duplicates include ", 
                 paste(names(quotations)[duplicated(names(quotations))], collapse = ", "), 
@@ -52,7 +51,7 @@ declare_time_warn <- function(message, declaration) {
 }
 
 declare_time_error_if_data <- function(declaration) {
-  if("data" %in% names(attr(declaration, "dots")))
+  if ("data" %in% names(attr(declaration, "dots")))
     declare_time_error("`data` should not be a declared argument.", declaration)
 }
 
@@ -62,7 +61,7 @@ declare_time_error_if_data <- function(declaration) {
 
 future_lapply <- function(..., future.seed = NA, future.globals=TRUE) {
   if (requireNamespace("future.apply", quietly = TRUE)) {
-    future.apply::future_lapply(..., future.seed=future.seed, future.globals = future.globals)
+    future.apply::future_lapply(..., future.seed = future.seed, future.globals = future.globals)
   } else {
     lapply(...)
   }
@@ -73,20 +72,20 @@ future_lapply <- function(..., future.seed = NA, future.globals=TRUE) {
 
 # If <= 5 uniques, table it, ow descriptives if numeric-ish, ow number of levels.
 describe_variable <- function(x) {
-
-  num_unique=length(unique(x))
-
-  if(num_unique > 5) {
+  
+  num_unique <- length(unique(x))
+  
+  if (num_unique > 5) {
     return(describe_variable_impl(x, num_unique))
   }
-
+  
   tab <- table(x, exclude = NULL)
-
+  
   rbind.data.frame(
-    Frequency=data.frame(as.list(tab), check.names = FALSE),
-    Proportion=sprintf("%.2f", prop.table(tab))
+    Frequency = data.frame(as.list(tab), check.names = FALSE),
+    Proportion = sprintf("%.2f", prop.table(tab))
   )
-
+  
 }
 
 
@@ -95,7 +94,7 @@ describe_variable_impl <- function(x, num_unique) UseMethod("describe_variable_i
 describe_variable_impl.factor <- function(x, num_unique) {
   data.frame(
     as.list(summary.factor(x, 5)),
-    N_unique=num_unique,
+    N_unique = num_unique,
     check.names = FALSE
   )
 }
@@ -114,8 +113,14 @@ describe_variable_impl.default <- function(x, num_unique) {
   #todo just use summary?
   data.frame(
     lapply(
-      list(min=min, median=median, mean=mean, max=max, sd=sd),
-      function(f,x) round(f(x, na.rm=TRUE), digits=2),
+      list(
+        min = min,
+        median = median,
+        mean = mean,
+        max = max,
+        sd = sd
+      ),
+      function(f, x) round(f(x, na.rm = TRUE), digits = 2),
       x
     ),
     N_missing = sum(is.na(x)),
@@ -131,18 +136,18 @@ describe_variable_impl.default <- function(x, num_unique) {
 rbind_disjoint <- function(list_of_df, infill=NA) {
   list_of_df <- Filter(is.data.frame, list_of_df)
   all_columns <- Reduce(union, lapply(list_of_df, colnames))
-
-  for(i in seq_along(list_of_df)) {
+  
+  for (i in seq_along(list_of_df)) {
     list_of_df[[i]][setdiff(all_columns, colnames(list_of_df[[i]]))] <- infill
   }
-
+  
   list_of_df <- lapply(list_of_df, `[`, all_columns)
-
+  
   do.call(rbind.data.frame, append(list_of_df, list(make.row.names = FALSE, stringsAsFactors = FALSE)))
 }
 
 add_parens <- function(x, digits = 3) {
-  sprintf("(%s)", format_num(x, digits)))
+  sprintf("(%s)", format_num(x, digits))
 }
 
 format_num <- function(x, digits = 3) {
@@ -153,13 +158,15 @@ format_num <- function(x, digits = 3) {
 check_sim_number <- function(simulations_df,
                              sims,
                              grouping_variables = c("design_ID",  "estimand_label", "estimator_label", "coefficient")) {
-
+  
   group_by_set  <- colnames(simulations_df) %i% grouping_variables
-  group_by_list <- simulations_df[, group_by_set, drop=FALSE]
+  group_by_list <- simulations_df[, group_by_set, drop = FALSE]
   check_df      <- split(group_by_list, group_by_list, drop = TRUE)
   check_sims    <- unlist(lapply(check_df, nrow)) != prod(sims)
-  if(any(check_sims))  warning(paste0("More simulations than expected for profiles:",
-                                      paste0(names(check_sims)[check_sims], collapse = ", ")))
+  if (any(check_sims)) {
+    warning(paste0("More simulations than expected for profiles:",
+                   paste0(names(check_sims)[check_sims], collapse = ", ")))
+  }
 }
 
 
@@ -172,7 +179,7 @@ get_added_variables <- function(last_df = NULL, current_df) {
 get_modified_variables <- function(last_df = NULL, current_df) {
   is_modified <- function(j) !isTRUE(all.equal(last_df[[j]], current_df[[j]]))
   shared <- intersect(names(current_df), names(last_df))
-
+  
   Filter(is_modified, shared)
 }
 
