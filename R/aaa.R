3d1096e322f1457b6efb2a88286e67958112d140
Graeme Blair <graemeblair@users.noreply.github.com>
2018-06-06 12:39:48 -0400

    whitespace

--- R/aaa.R
+++ R/aaa.R
@@ -2,10 +2,10 @@
 # Copies an environment chain
 #' @importFrom rlang env_clone
 env_deep_copy <- function(e) {
-  if(environmentName(e) == "CheckExEnv") e else # Cloning the CheckExEnv causes examples to autofail, it has delayedAssign("F", stop())
-  if(identical(e, emptyenv())) emptyenv() else
-    if(identical(e, globalenv())) env_clone(e) else # don't clone attached packages
-      env_clone(e, Recall(parent.env(e)))
+  if (environmentName(e) == "CheckExEnv") e else # Cloning the CheckExEnv causes examples to autofail, it has delayedAssign("F", stop())
+    if (identical(e, emptyenv())) emptyenv() else
+      if (identical(e, globalenv())) env_clone(e) else # don't clone attached packages
+        env_clone(e, Recall(parent.env(e)))
 }
 
 ###############################################################################
@@ -15,10 +15,10 @@ env_deep_copy <- function(e) {
 
 dots_env_copy <- function(dots) {
   eprev <- NULL
-  for(i in seq_along(dots)) {
+  for (i in seq_along(dots)) {
     ecurrent <- environment(dots[[i]])
-    if(!is.null(ecurrent)) {
-      if(!identical(ecurrent, eprev) ) {
+    if (!is.null(ecurrent)) {
+      if (!identical(ecurrent, eprev)) {
         eprev <- ecurrent
         eclone <- env_deep_copy(ecurrent)
       }
@@ -32,45 +32,53 @@ dots_env_copy <- function(dots) {
 # Given a function and dots, rename dots based on how things will positionally match
 #' @importFrom rlang is_empty
 rename_dots <- function(handler, dots, addData=TRUE){
-  if(is_empty(dots)) return(dots)
+  if (is_empty(dots)) {
+    return(dots)
+  }
+  
   f <- function(...) as.list(match.call(handler)[-1])
-
+  
   d_idx <- setNames(seq_along(dots), names(dots))
-
-  if(addData) d_idx["data"] <- list(NULL)
-
+  
+  if (addData) {
+    d_idx["data"] <- list(NULL)
+  }
+  
   d_idx <- eval_tidy(quo(f(!!!d_idx)))
-
-  if(addData) d_idx$data <- NULL
-
+  
+  if (addData) {
+    d_idx$data <- NULL
+  }
+  
   d_idx <- unlist(d_idx)
-
+  
   d_idx <- d_idx[names(d_idx) != "" ]
-
+  
   names(dots)[d_idx] <- names(d_idx)
-
+  
   dots
 }
 
 # Returns a new function(data) which calls FUN(data, dots)
-currydata <- function(FUN, dots, addDataArg=TRUE,strictDataParam=TRUE, cloneDots=TRUE) {
+currydata <- function(FUN, dots, addDataArg = TRUE, strictDataParam = TRUE, cloneDots = TRUE) {
   # heuristic to reuse deep clones
-  if(cloneDots) dots <- dots_env_copy(dots)
-
-  quoNoData <- quo((FUN)(!!!dots))
-
-  if(addDataArg && !'data' %in% names(dots) && !'.data' %in% names(dots)){
-
-    dots <- append(dots, list(data=quote(data)), after=FALSE)
+  if (cloneDots) {
+    dots <- dots_env_copy(dots)
   }
-
+  
+  quoNoData <- quo((FUN)(!!!dots))
+  
+  if (addDataArg && !'data' %in% names(dots) && !'.data' %in% names(dots)) {
+    dots <- append(dots, list(data = quote(data)), after = FALSE)
+  }
+  
   quo <- quo((FUN)(!!!dots))
-
-  if(isTRUE(strictDataParam)) function(data) eval_tidy(quo, data=list(data=data))
+  
+  if (isTRUE(strictDataParam)) function(data) eval_tidy(quo, data = list(data = data))
   else  function(data=NULL){
     #message(quo)
     #used for declare_population with no seed data provided
-    res <- if(is.null(data)) eval_tidy(quoNoData) else eval_tidy(quo, data=list(data=data))
+    res <- if (is.null(data)) eval_tidy(quoNoData) else eval_tidy(quo, data = list(data = data))
     res
   }
 }
@@ -81,56 +89,67 @@ currydata <- function(FUN, dots, addDataArg=TRUE,strictDataParam=TRUE, cloneDots
 #' @importFrom rlang enquo
 declaration_template <- function(..., handler, label=NULL){
   #message("Declared")
-
-  dots <- quos(...,label=!!label)
+  
+  dots <- quos(..., label = !!label)
   this <- attributes(sys.function())
-
-  if(!"label" %in% names(formals(handler))){
+  
+  if (!"label" %in% names(formals(handler))) {
     dots$label <- NULL
   }
-
+  
   dots <- rename_dots(handler, dots, this$strictDataParam)
   dots <- dots_env_copy(dots)
-
-
-  ret <- build_step(currydata(handler, dots, strictDataParam=this$strictDataParam, cloneDots = FALSE),
-            handler=handler,
-            dots=dots,
-            label=label,
-            step_type=this$step_type,
-            causal_type=this$causal_type,
-            call=match.call())
-
-  if(has_validation_fn(handler)) ret <- validate(handler, ret,  dots, label)
+  
+  
+  ret <- build_step(currydata(handler,
+                              dots,
+                              strictDataParam = this$strictDataParam,
+                              cloneDots = FALSE),
+                    handler = handler,
+                    dots = dots,
+                    label = label,
+                    step_type = this$step_type,
+                    causal_type = this$causal_type,
+                    call = match.call())
+  
+  if (has_validation_fn(handler)) {
+    ret <- validate(handler, ret,  dots, label)
+  }
 
   ret
 }
 
 # data structure for steps
 build_step <- function(curried_fn, handler, dots, label, step_type, causal_type, call){
-  structure(curried_fn,
-            handler=handler,
-            dots=dots,
-            label=label,
-            step_type=step_type,
-            causal_type=causal_type,
-            call=call,
-            class=c("design_step", "dd","function"))
+  structure(
+    curried_fn,
+    handler = handler,
+    dots = dots,
+    label = label,
+    step_type = step_type,
+    causal_type = causal_type,
+    call = call,
+    class = c("design_step", "dd", "function")
+  )
 }
 
 # generate declaration steps (eg declare_population) by setting the default handler and metadata
 make_declarations <- function(default_handler, step_type, causal_type='dgp', default_label, strictDataParam=TRUE) {
-
+  
   declaration <- declaration_template
-
+  
   formals(declaration)$handler <- substitute(default_handler)
-  if(!missing(default_label)) formals(declaration)$label <- default_label
-
-  structure(declaration,
-            class=c('declaration', 'function'),
-            step_type=step_type,
-            causal_type=causal_type,
-            strictDataParam=strictDataParam)
+  if (!missing(default_label)) {
+    formals(declaration)$label <- default_label
+  }
+  
+  structure(
+    declaration,
+    class = c('declaration', 'function'),
+    step_type = step_type,
+    causal_type = causal_type,
+    strictDataParam = strictDataParam
+  )
 }
 
 ###############################################################################
