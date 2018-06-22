4942d6222d42bac4f6dc10deb291c4691b719baa
Graeme Blair <graemeblair@users.noreply.github.com>
2018-06-22 16:06:23 -0700

    comment out the df sims functionality

--- tests/testthat/test-fanout.R
+++ tests/testthat/test-fanout.R
@@ -84,18 +84,17 @@ test_that("sims expansion is correct",{
 })
 
 
-test_that("fan_out ids are correct",{
-  sleep_large <- resample_data(sleep, N = 200)
-  design <- declare_population(sleep_large) +
-    declare_estimand(2, label = "a") +
-    declare_estimand(b = rnorm(1))
-  
-  fan <- data.frame(end = 1:3, n = c(5, 5, 5))
-
-  debugonce(diagnose_design)
-  fo <- diagnose_design(design, sims = fan)
-
-  fo_id <- fo$simulations[paste0("fan_", 1:3)]
-
-  expect_equal(nrow(unique(fo_id)), prod(fan$n))
-})
+# test_that("fan_out ids are correct",{
+#   sleep_large <- resample_data(sleep, N = 200)
+#   design <- declare_population(sleep_large) +
+#     declare_estimand(2, label = "a") +
+#     declare_estimand(b = rnorm(1))
+#   
+#   fan <- data.frame(end = 1:3, n = c(5, 5, 5))
+# 
+#   fo <- diagnose_design(design, sims = fan)
+# 
+#   fo_id <- fo$simulations[paste0("fan_", 1:3)]
+# 
+#   expect_equal(nrow(unique(fo_id)), prod(fan$n))
+# })
