76a02412cc823614d20bd86193334d00720ec5e5
Graeme Blair <graemeblair@users.noreply.github.com>
2018-06-21 19:48:05 -0700

    tests

--- tests/testthat/test-fanout.R
+++ tests/testthat/test-fanout.R
@@ -85,11 +85,11 @@ test_that("sims expansion is correct",{
 
 test_that("fan_out ids are correct",{
   sleep_large <- resample_data(sleep, N = 200)
-  design <- declare_population(sleep) +
+  design <- declare_population(sleep_large) +
     declare_estimand(2, label = "a") +
     declare_estimand(b = rnorm(1))
   
-  fan <- data.frame(end = 1:3, n = c(2, 3, 5))
+  fan <- data.frame(end = 1:3, n = c(5, 5, 5))
 
   fo <- diagnose_design(design, sims = fan)
 
