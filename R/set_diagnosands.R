c439c3559252ce6056ea10835d16c6ac9ab5facb
Graeme Blair <graemeblair@users.noreply.github.com>
2018-06-24 22:07:30 -0700

    bug fix

--- R/set_diagnosands.R
+++ R/set_diagnosands.R
@@ -13,14 +13,16 @@
 #' declare_population(data = sleep) + 
 #'   declare_estimand(mean_outcome = mean(extra)) +
 #'   declare_sampling(n = 10) + 
-#'   declare_estimator(extra ~ 1, estimand = mean_outcome) 
+#'   declare_estimator(extra ~ 1, estimand = "mean_outcome", coefficients = '(Intercept)', model = lm_robust) 
 #' 
 #' diagnosands <- declare_diagnosands(
 #'   median_bias = median(est - estimand), keep_defaults = FALSE)
 #' 
 #' design <- set_diagnosands(design, diagnosands)
 #' 
+#' \notrun{
 #' diagnose_design(design)
+#' }
 #' 
 #' @export
 set_diagnosands <- function(design, diagnosands = default_diagnosands) {
