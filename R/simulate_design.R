d0a8a95055050a1cbfa8be1393e2ea30485d7dce
acoppock <acoppock@gmail.com>
2018-08-21 13:54:29 -0600

    fix #316

--- R/simulate_design.R
+++ R/simulate_design.R
@@ -172,10 +172,13 @@ simulate_single_design <- function(design, sims) {
       data.frame(
         simulations_df[, 1, drop = FALSE],
         as_list(attr(design, "parameters")),
-        simulations_df[, -1, drop = FALSE]
+        simulations_df[, -1, drop = FALSE], 
+        stringsAsFactors = FALSE
       )
   }
 
+  simulations_df <- data.frame(lapply(simulations_df, type_convert), stringsAsFactors = FALSE)
+  
   simulations_df
 }
 
