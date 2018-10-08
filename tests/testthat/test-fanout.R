d3f670835f6c08c6e52e0099e6cb112ef03a2ec9
acoppock <acoppock@gmail.com>
2018-10-08 11:27:04 -0600

    add small test

--- tests/testthat/test-fanout.R
+++ tests/testthat/test-fanout.R
@@ -155,6 +155,7 @@ test_that("correct fan out", {
     simulate_design(declare_population(sleep) + e1 + e2 + e3, sims = c(30, 1, 5, 2))
   
   expect_equivalent(apply(out[,c(5:7)], 2, max), c(30, 150, 300))
+  expect_equivalent(tapply(out$estimand, INDEX = out$estimand_label, max), c(30, 150, 300))
   
 })
 
