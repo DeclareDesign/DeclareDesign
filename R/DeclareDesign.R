2d2022cdbbcffce8ec645931ef9e82c162cb5935
Graeme Blair <graemeblair@users.noreply.github.com>
2018-10-21 09:20:14 -0700

    add tab completion from DeclareDesign/completion

--- R/DeclareDesign.R
+++ R/DeclareDesign.R
@@ -44,6 +44,65 @@
 #' @name DeclareDesign
 NULL
 
+# tab completion
+
+comp_env <- list2env(list(completer = NULL), parent = emptyenv());
+
+.onAttach <- function(libname, pkgname){
+
+  if(requireNamespace("utils", quietly = FALSE)){
+    comp_env$completer <- utils::rc.getOption("custom.completer")
+
+
+    rc.options("custom.completer"= DDcompletor)
+
+  }
+
+}
+
+.onDetach <- function(libPath){
+  rc.options(custom.completer = comp_env$completer)
+}
+
+# Sneaks around import checks ? see also jimhest/completeme on github
+complete_token <- get(".completeToken", asNamespace("utils"))
+
+DDcompletor <- function(CompletionEnv){
+
+  on.exit( rc.options("custom.completer" = DDcompletor) )
+  rc.options(custom.completer = comp_env$completer)
+  ret <- complete_token() #fills in CompletionEnv data
+
+  if(is.character(CompletionEnv$fguess) && length(CompletionEnv$fguess) > 0 && !any(grepl("handler", CompletionEnv$linebuffer))){
+    FUN <- get0(CompletionEnv$fguess, asNamespace("DeclareDesign"), mode = "function")
+
+    if(is.function(FUN) && inherits(FUN, "declaration")){
+
+      h <- eval(formals(FUN)$handler, environment(FUN))
+
+      h <- union(names(formals(FUN)), names(formals(h)))
+
+      h <- setdiff(h, "data")
+
+      CompletionEnv$comps = paste0(h, "=")
+
+      ## TODO port to startsWith
+      if(length(CompletionEnv$token) == 1 && nchar(CompletionEnv$token) > 0){
+        w <- regexpr(CompletionEnv$token, h) == 1
+        if(sum(w) == 1) {
+          CompletionEnv$comps = h[w]
+
+          return(h[w])
+
+        }
+      }
+
+      return(h)
+    }
+  }
+
+  ret
+}
 
 .onLoad <- function(libname, pkgname) {
   repos <- getOption("repos")
