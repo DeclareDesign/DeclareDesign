c6bd1acd9cec0e2757f6d89c751f98a81926af69
Graeme Blair <graemeblair@users.noreply.github.com>
2019-01-18 19:59:27 -0800

    documentation

--- R/draw_functions.R
+++ R/draw_functions.R
@@ -1,5 +1,27 @@
-
-#' @rdname post_design
+#'  Draw data, estimates, and estimands from a design
+#'  
+#' @param design A design object, typically created using the + operator
+#' @param end (Defaults to \code{length(design)}) a scalar indicating which step in the design to finish drawing data by.
+#'
+#' @name draw_functions
+#' 
+#' @examples 
+#' 
+#' design <- declare_population(N = 100, u = rnorm(N)) +
+#'   declare_potential_outcomes(Y ~ Z + u) +
+#'   declare_estimand(ATE = mean(Y_Z_1 - Y_Z_0)) +
+#'   declare_sampling(n = 75) +
+#'   declare_assignment(m = 50) +
+#'   declare_reveal(Y, Z) +
+#'   declare_estimator(Y ~ Z, estimand = "ATE")
+#' 
+#' dat <- draw_data(design)
+#' 
+#' dat_no_sampling <- draw_data(design, end = 3)
+#' 
+#' draw_estimands(design)
+#' 
+#' draw_estimates(design)
 #'
 #' @export
 draw_data <- function(design, end = length(design)) {
@@ -7,15 +29,16 @@ draw_data <- function(design, end = length(design)) {
     stop("Please provide an end step as a number between 1 and the total number of steps in the design.")
   }
   
-  run_design_internal(design[1:end], results = list(current_df = 0))$current_df
+  run_design_internal.design(design[1:end], results = list(current_df = 0))$current_df
 }
 
-#' @rdname post_design
+#' @param ... A design or set of designs typically created using the + operator
+#' @rdname draw_functions
 #'
 #' @export
 draw_estimands <- function(...) apply_on_design_dots(draw_estimands_single_design, ...)
 
-#' @rdname post_design
+#' @rdname draw_functions
 #'
 #' @export
 draw_estimates <- function(...) apply_on_design_dots(draw_estimates_single_design, ...)
