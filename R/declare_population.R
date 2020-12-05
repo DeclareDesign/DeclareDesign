f46d228ee3fb602b48c089ca0b77274ee88450c6
Alexander Coppock <acoppock@gmail.com>
2020-12-05 18:51:05 -0500

    move around vignettes

--- R/declare_population.R
+++ R/declare_population.R
@@ -7,39 +7,41 @@
 #'
 #' @examples
 #'
-#' # Default handler is fabricatr::fabricate
-#'
 #' # Declare a single-level population with no covariates
+#' declare_population(N = 100)
+#' 
+#' # declare_population returns a function:
+#' 
 #' my_population <- declare_population(N = 100)
+#' #' my_population()
 #'
 #' # Declare a population from existing data
-#' my_population <- declare_population(sleep)
+#' declare_population(sleep)
 #'
 #' # Declare a single-level population with a covariate
-#' my_population <- declare_population(
+#' declare_population(
 #'   N = 6,
-#'   gender = draw_binary(N, prob = 0.5),
-#'   height_ft = rnorm(N, mean = 5 + 4/12 + 4/12 * gender, sd = 3/12)
+#'   female = rbinom(n = N, 1, prob = 0.5),
+#'   height_ft = rnorm(N, mean = 5.67 - 0.33 * female, sd = 0.25)
 #' )
-#' my_population()
-#'
+#' 
 #' # Declare a two-level hierarchical population
 #' # containing cities within regions and a
 #' # pollution variable defined at the city level
 #'
-#' my_population <- declare_population(
+#' declare_population(
 #'   regions = add_level(N = 5),
 #'   cities = add_level(N = 10, pollution = rnorm(N, mean = 5))
 #' )
-#' my_population()
 #'
 #' # Declare a population using a custom function
+#' 
+#' the default handler is fabricatr::fabricate, but you can supply any function that returns a data.frame
 #'
 #' my_population_function <- function(N) {
 #'   data.frame(u = rnorm(N))
 #' }
 #'
-#' my_population_custom <- declare_population(N = 10, handler = my_population_function)
+#' declare_population(N = 10, handler = my_population_function)
 #'
-#' my_population_custom()
 declare_population <- make_declarations(fabricate, "population")
