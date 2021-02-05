912310b3217fe11bba8dcbf0e1a2ca865a7999c7
Alexander Coppock <acoppock@gmail.com>
2021-02-05 13:31:43 -0500

    fixing clobbered / incomplete examples

--- R/declare_model.R
+++ R/declare_model.R
@@ -6,40 +6,63 @@
 #' @importFrom fabricatr fabricate
 #'
 #' @examples
-#'
-#' # Default handler is fabricatr::fabricate
-#'
+#' 
 #' # Declare a single-level population with no covariates
-#' my_population <- declare_model(N = 100)
-#'
-#' # Declare a population from existing data
-#' my_population <- declare_model(sleep)
-#'
-#' # Declare a single-level population with a covariate
-#' my_population <- declare_model(
+#' declare_model(N = 100)
+#' 
+#' # declare_model returns a function:
+#' 
+#' my_model <- declare_model(N = 100)
+#' my_model()
+#' 
+#' # Declare a single-level population with two covariates
+#' declare_model(
 #'   N = 6,
-#'   gender = draw_binary(N, prob = 0.5),
-#'   height_ft = rnorm(N, mean = 5 + 4/12 + 4/12 * gender, sd = 3/12)
+#'   female = rbinom(n = N, 1, prob = 0.5),
+#'   height_ft = rnorm(N, mean = 5.67 - 0.33 * female, sd = 0.25)
 #' )
-#' my_population()
-#'
+#' 
+#' # Declare a single-level population with potential outcomes
+#' declare_model(
+#'   N = 6, 
+#'   U = rnorm(N),
+#'   potential_outcomes(Y ~ Z + U))
+#' 
+#' 
+#' # Declare a single-level population with two sets of potential outcomes
+#' declare_model(
+#'   N = 6,
+#'   U = rnorm(N),
+#'   potential_outcomes(Y ~ Z1 + Z2 + U, 
+#'                      conditions = list(Z1 = c(0, 1), Z2 = c(0, 1))))
+#' 
+#' 
+#' 
+#' 
+#' # Declare a population from existing data
+#' 
+#' declare_model(mtcars)
+#' 
+#' # Resample from existing data
+#' 
+#' declare_model(N = 100, data = mtcars, handler = resample_data)
+#' 
+#' 
 #' # Declare a two-level hierarchical population
 #' # containing cities within regions and a
 #' # pollution variable defined at the city level
-#'
-#' my_population <- declare_model(
-#'   regions = add_level(N = 5),
-#'   cities = add_level(N = 10, pollution = rnorm(N, mean = 5))
-#' )
-#' my_population()
-#'
+#' 
+#' declare_model(regions = add_level(N = 5),
+#'               cities = add_level(N = 10, pollution = rnorm(N, mean = 5)))
+#' 
 #' # Declare a population using a custom function
-#'
-#' my_population_function <- function(N) {
+#' 
+#' # the default handler is fabricatr::fabricate,
+#' # but you can supply any function that returns a data.frame
+#' 
+#' my_model_function <- function(N) {
 #'   data.frame(u = rnorm(N))
 #' }
-#'
-#' my_population_custom <- declare_model(N = 10, handler = my_population_function)
-#'
-#' my_population_custom()
+#' 
+#' declare_model(N = 10, handler = my_model_function)
 declare_model <- make_declarations(fabricate, "model")
