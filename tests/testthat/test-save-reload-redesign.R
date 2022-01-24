950bcc475cf24b73c3b0ac7b73eec0a4883133dc
Graeme Blair <graemeblair@users.noreply.github.com>
2022-01-24 12:30:18 -0800

    test that breaks it

--- tests/testthat/test-save-reload-redesign.R
+++ tests/testthat/test-save-reload-redesign.R
@@ -21,3 +21,28 @@ test_that("Save Reload Redesign works", {
 
   expect_true(is.data.frame(draw_data(d2)))
 })
+
+test_that("Save Reload Redesign works with lm_robust", {
+  library(testthat)
+  library(DeclareDesign)
+  library(tidyverse)
+  N <- 100
+  d <- declare_model(N = N, foo = rnorm(N)) + declare_inquiry(d = mean(foo)) + declare_estimator(foo ~ 1, model = lm_robust)
+  f <- "~/desktop/tmp.RDS"
+  
+  suppressWarnings(
+    write_rds(d, file = f)
+  )
+  
+  rm(d, N)
+  
+  suppressWarnings(
+    d <- readRDS(f)
+  )
+  
+  expect_true(is.data.frame(run_design(d)))
+  
+  d2 <- redesign(d, N = 20)
+  
+  expect_true(is.data.frame(run_design(d2)))
+})
