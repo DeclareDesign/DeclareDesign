58a9244fc4f8ad0a9bba4bc97ec856c20555ba14
Alex Coppock <acoppock@gmail.com>
2022-04-07 20:28:55 -0400

    measurement

--- R/declare_measurement.R
+++ R/declare_measurement.R
@@ -11,28 +11,55 @@
 #'
 #' @examples
 #' 
+#' # declare_measurement in use
+#' ## Two-arm randomized experiment
 #' design <-
-#'   declare_model(N = 6,
-#'                 U = rnorm(N),
-#'                 potential_outcomes(Y ~ Z + U)) +
-#'   declare_assignment(Z = complete_ra(N)) + 
+#'   declare_model(
+#'     N = 500,
+#'     X = rep(c(0, 1), each = N / 2),
+#'     U = rnorm(N, sd = 0.25),
+#'     potential_outcomes(Y ~ 0.2 * Z + X + U)
+#'   ) +
+#'   declare_inquiry(ATE = mean(Y_Z_1 - Y_Z_0)) +
+#'   declare_sampling(S = complete_rs(N = N, n = 200)) +
+#'   declare_assignment(Z = complete_ra(N = N, m = 100)) +
+#'   declare_measurement(Y = reveal_outcomes(Y ~ Z)) +
+#'   declare_estimator(Y ~ Z, inquiry = "ATE")
+#' 
+#' # Reveal potential outcomes according to treatment assignment
+#' design <-
+#'   declare_model(N = 100,
+#'                 potential_outcomes(Y ~ rbinom(
+#'                   N, size = 1, prob = 0.1 * Z + 0.5
+#'                 ))) +
+#'   declare_assignment(Z = complete_ra(N, m = 50)) +
 #'   declare_measurement(Y = reveal_outcomes(Y ~ Z))
 #' 
-#' draw_data(design)
+#' # Generate observed measurement from a latent value
+#' design <- 
+#'   declare_model(N = 100, latent = runif(N)) +
+#'   declare_measurement(observed = rbinom(N, 1, prob = latent))
 #' 
+#' # Index creation
+#' library(psych)
 #' 
 #' design <-
 #'   declare_model(
-#'     N = 6,
-#'     U = rnorm(N),
-#'     potential_outcomes(Y ~ Z1 + Z2 + U, 
-#'                        conditions = list(Z1 = c(0, 1), Z2 = c(0, 1)))) +
-#'   declare_assignment(Z1 = complete_ra(N), 
-#'                      Z2 = block_ra(blocks = Z1),
-#'                      legacy = FALSE) + 
-#'   declare_measurement(Y = reveal_outcomes(Y ~ Z1 + Z2))
+#'     N = 500,
+#'     X = rep(c(0, 1), each = N / 2),
+#'     Y_1 = 0.2 * X + rnorm(N, sd = 0.25),
+#'     Y_2 = 0.3 * X + 0.5 * rnorm(N, sd = 0.50),
+#'     Y_3 = 0.1 * X + 0.4 * rnorm(N, sd = 0.75)) +
+#'   declare_measurement(
+#'     index = fa(
+#'       r = cbind(Y_1, Y_2, Y_3),
+#'       nfactors = 1,
+#'       rotate = "varimax"
+#'     )$scores
+#'   )
 #' 
 #' draw_data(design)
+#' 
 #' 
 #' 
 #'
