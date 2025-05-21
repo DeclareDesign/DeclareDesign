dc4cc25451e2560a3ac359bd816559af68df9b31
macartan <macartan@gmail.com>
2025-05-21 18:22:41 +0200

    redesign on lists

--- tests/testthat/test-redesign.R
+++ tests/testthat/test-redesign.R
@@ -1,6 +1,6 @@
 context("redesign")
 
-test_that("N not changed", {
+test_that("N changing", {
   N <- 100
   design <- declare_model(N = N) + NULL
   expect_equal(N, 100)
@@ -23,4 +23,43 @@ test_that("N not changed", {
 
   # N itself should not be changed
   expect_equal(N, 100)
+})
+
+
+test_that("message if meaningless change", {
+  N <- 100
+  design <- declare_model(N = N) + NULL
+  expect_message(redesign(design, k = 2, N = 200), "You requested a change to k but k is not found in the design")
+})
+
+
+
+test_that("embedded parameter, multiple steps", {
+  n <- 1000
+  b <- .2
+  
+  d <- 
+    declare_model(N = n, Y = rnorm(N, b)) + 
+    declare_estimator(Y ~ 1) +
+    declare_inquiry(Q = b)
+  
+  expect_true(run_design(d)$estimand == .2)
+  
+  rm(b)
+  
+  expect_true(run_design(d)$estimand == .2)
+  
+  d2 <- redesign(d, b = .4)
+  x <- run_design(d2)
+  expect_true(x$estimand == .4)
+  expect_true(abs(x$estimand - x$estimate) < .1)
+  attr(d2[[1]], "dots")
+  
+  environment(attr(d2[[1]], "dots")$Y)$b
+  
+  d3 <- redesign(d, b = 10)
+  x <- run_design(d3)
+  expect_true(x$estimand == 10)
+  expect_true(abs(x$estimand - x$estimate) < .1)
+  
 })
