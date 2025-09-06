5056e13badd595e9aedf1a6d48d965f05fdc4a3e
Macartan Humphreys <macartan@gmail.com>
2025-09-06 12:20:31 +0200

    typos

--- R/declare_test.R
+++ R/declare_test.R
@@ -1,6 +1,6 @@
 #' Declare test
 #'
-#' @description Declares an test which generates a test statistic and associated inferential statistics. 
+#' @description Declares a test which generates a test statistic and associated inferential statistics. 
 #' 
 #' Use of \code{declare_test} is identical to use of \code{\link{declare_estimator}}. Use \code{declare_test} for hypothesis testing with no specific inquiry in mind; use \code{declare_estimator} for hypothesis testing when you can link each estimate to an inquiry. For example, \code{declare_test} could be used for a K-S test of distributional equality and \code{declare_estimator} for a difference-in-means estimate of an average treatment effect.
 #' 
